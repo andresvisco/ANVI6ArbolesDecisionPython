a4fff6cdf631b8f40aa14630b5a4f98c1029cb7b
Fernando Vazquez <>
2019-02-20 17:04:49 -0300

    ? updated for Color Tree

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from sklearn.datasets import load_iris
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-from sklearn.externals.six import StringIO  
-from IPython.display import Image  
+from sklearn.externals.six import StringIO
+from IPython.display import Image
 from sklearn.tree import export_graphviz
 import pydot
 import pydotplus
@@ -34,7 +34,37 @@ dtree.fit(df, y)
 # dot_data = StringIO()
 dot_data = tree.export_graphviz(dtree, out_file=None,filled=True,rounded=True,feature_names=feature_cols)
 # graph = pydot.graph_from_dot_data(dot_data.getvalue())
-graph = graphviz.Source(dot_data)
-print(graph)
-graph.render()
+# graph = graphviz.Source(dot_data)
+# print(graph)
+# graph.render()
 
+graph = pydotplus.graph_from_dot_data(dot_data)
+nodes = graph.get_node_list()
+diffVal=0
+for node in nodes:
+    if node.get_label():
+        values = node.get_label().split("\\n")
+        new_text_values = []
+        for v in values:
+            new_text_values.append(v)
+            if v.__contains__('DIFFMinutos <= '):
+                pf = v.replace('\"DIFFMinutos <= ', '')
+                diffVal = float(pf)
+
+            if v.__contains__('samples'):
+                break
+        nt = "\n".join(new_text_values)
+        node.set_label(nt)
+
+        if diffVal<-60:
+            node.set_fillcolor("red")
+        elif diffVal<-30:
+            node.set_fillcolor("orange")
+        elif diffVal<-15:
+            node.set_fillcolor("yellow")
+        elif diffVal<-1:
+            node.set_fillcolor("blue")
+        else:
+            node.set_fillcolor("forestgreen")
+
+graph.write_png('colored_tree.png')
